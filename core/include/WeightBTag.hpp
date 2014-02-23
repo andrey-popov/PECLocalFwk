3128b532344deb63d69538aed2a96b0fe8f1fd94
Andrey Popov <rainfinder@gmail.com>
2014-02-23 20:20:04 +0100

    Update implementation of BTagWeight to match new accessors to b-tagging efficiencies and scale factors
    This is a draft implementation.

--- core/include/WeightBTag.hpp
+++ core/include/WeightBTag.hpp
@@ -2,16 +2,16 @@
  * \file WeightBTag.hpp
  * \author Andrey Popov
  * 
- * The module defines a class to account for b-tagging scale factors.
+ * Defines a class to evaluate event weight due to b-tagging.
  */
 
 #pragma once
 
-#include <BTagger.hpp>
-#include <BTagDatabase.hpp>
-#include <PhysicsObjects.hpp>
+#include <WeightBTagInterface.hpp>
 
-#include <vector>
+#include <BTagger.hpp>
+#include <BTagEffInterface.hpp>
+#include <BTagSFInterface.hpp>
 
 
 /**
@@ -26,52 +26,41 @@
  * no need to specify the event selection to this class.
  * [1] https://twiki.cern.ch/twiki/bin/view/CMS/BTagSFMethods#1a_Event_reweighting_using_scale
  */
-class WeightBTag
+class WeightBTag: public WeightBTagInterface
 {
-    public:
-        /// Supported systematical variations
-        enum class Variation
-        {
-            Central,  ///< Nominal weight
-            TagRateUp,  ///< Scale factors for b- and c-jets increased
-            TagRateDown,  ///< Scale factors for b- and c-jets decreased
-            MistagRateUp,  ///< Scale factors for light-flavour and gluon jets increased
-            MistagRateDown  ///< Scale factors for light-flavour and gluon jets decreased
-        };
+public:
+    /**
+     * \brief Constructor
+     */
+    WeightBTag(BTagger const *bTagger, BTagEffInterface *efficiencies,
+     BTagSFInterface *scaleFactors);
+
+public:
+    /**
+     */
+    virtual WeightBTagInterface *Clone() const;
     
-    public:
-        WeightBTag(BTagger const &bTagger_, BTagDatabase const &bTagDatabase_);
+    /**
+     */
+    virtual void LoadPayload(Dataset const &dataset);
     
-    public:
-        /// Sets the default variation
-        void SetDefaultVariation(Variation var);
-        
-        /**
-         * \brief Calculates event weight
-         * 
-         * The only difference from the sibling method CalcWeight is that this one uses the default
-         * variation.
-         */
-        double CalcWeight(std::vector<Jet> const &jets) const;
-        
-        /**
-         * \brief Calculates event weight
-         * 
-         * Calculates an event weight needed to account for b-tagging scale factors. The tags
-         * configuration is not modified: if a jet is b-tagged, it is considered as b-tagged for
-         * both MC and data.
-         */
-        double CalcWeight(std::vector<Jet> const &jets, Variation var) const;
+    /**
+     * \brief Calculates event weight
+     * 
+     * Calculates an event weight needed to account for b-tagging scale factors. The tags
+     * configuration is not modified: if a jet is b-tagged, it is considered as b-tagged for
+     * both MC and data.
+     */
+    virtual double CalcWeight(std::vector<Jet> const &jets, Variation var = Variation::Nominal)
+     const;
+
+private:
+    /// An object to choose b-tagged jets
+    BTagger const *bTagger;
     
-    private:
-        /// Reinterprets a variation in terms of BTagDatabase::SFVar enumeration
-        BTagDatabase::SFVar TranslateVariation(Variation var, int jetPDGID) const;
+    /// An object to access b-tagging efficiencies
+    BTagEffInterface *efficiencies;
     
-    private:
-        /// An object to choose b-tagged jets
-        BTagger const &bTagger;
-        /// An object to access b-tagging efficiencies and scale factors
-        BTagDatabase const &bTagDatabase;
-        /// Default variation
-        Variation defaultVar;
+    /// An object to access b-tagging scale factors
+    BTagSFInterface *scaleFactors;
 };
--- core/src/WeightBTag.cpp
+++ core/src/WeightBTag.cpp
@@ -4,25 +4,26 @@
 using namespace std;
 
 
-WeightBTag::WeightBTag(BTagger const &bTagger_, BTagDatabase const &bTagDatabase_):
-    bTagger(bTagger_), bTagDatabase(bTagDatabase_),
-    defaultVar(Variation::Central)
+WeightBTag::WeightBTag(BTagger const *bTagger_, BTagEffInterface *efficiencies_,
+ BTagSFInterface *scaleFactors_):
+    bTagger(bTagger_), efficiencies(efficiencies_), scaleFactors(scaleFactors_)
 {}
 
 
-void WeightBTag::SetDefaultVariation(Variation var)
+WeightBTagInterface *WeightBTag::Clone() const
 {
-    defaultVar = var;
+    // This is a placeholder!
+    return nullptr;
 }
 
 
-double WeightBTag::CalcWeight(vector<Jet> const &jets) const
+void WeightBTag::LoadPayload(Dataset const &dataset)
 {
-    return CalcWeight(jets, defaultVar);
+    efficiencies->LoadPayload(dataset);
 }
 
 
-double WeightBTag::CalcWeight(vector<Jet> const &jets, Variation var) const
+double WeightBTag::CalcWeight(vector<Jet> const &jets, Variation var /*=Variation::Nominal*/) const
 {
     // Logarithms of probabilities that the given tag configuration takes place with data-like or
     //MC-like b-tagging efficiencies. Since the probabilities for several jets will be multiplied,
@@ -33,12 +34,17 @@ double WeightBTag::CalcWeight(vector<Jet> const &jets, Variation var) const
     // Loop over the jets
     for (auto const &jet: jets)
     {
+        // Skip jets outside the tracker acceptance
+        if (fabs(jet.Eta()) > BTagSFInterface::GetMaxPseudorapidity())
+            continue;
+        
+        
         // Precalculate b-tagging efficiency and scale factor with the current jet
-        double const eff = bTagDatabase.GetEfficiency(jet);
-        double const sf = bTagDatabase.GetScaleFactor(jet,
+        double const eff = efficiencies->GetEfficiency(jet);
+        double const sf = scaleFactors->GetScaleFactor(jet,
          TranslateVariation(var, jet.GetParentID()));
         
-        if (bTagger.IsTagged(jet))
+        if (bTagger->IsTagged(jet))
         {
             logProbMC += log(eff);
             logProbData += log(eff * sf);
@@ -56,29 +62,4 @@ double WeightBTag::CalcWeight(vector<Jet> const &jets, Variation var) const
     
     // This is it. Much simpler than in previous version of the algorithm
     return exp(logProbData - logProbMC);
-}
-
-
-BTagDatabase::SFVar WeightBTag::TranslateVariation(Variation var, int jetPDGID) const
-{
-    unsigned const absFlavour = abs(jetPDGID);
-    
-    if (absFlavour == 5 or absFlavour == 4)
-    {
-        if (var == Variation::TagRateUp)
-            return BTagDatabase::SFVar::Up;
-        else if (var == Variation::TagRateDown)
-            return BTagDatabase::SFVar::Down;
-    }
-    else  // light-flavour and gluon jets
-    {
-        if (var == Variation::MistagRateUp)
-            return BTagDatabase::SFVar::Up;
-        else if (var == Variation::MistagRateDown)
-            return BTagDatabase::SFVar::Down;
-    }
-    
-    // If control has reached this point, it is either pure nominal or mismatched variation (like
-    //requesting TagRateUp for a gluon jet)
-    return BTagDatabase::SFVar::Central;
 }
