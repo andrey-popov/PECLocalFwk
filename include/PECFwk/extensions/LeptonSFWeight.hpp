64906875a7376fb454d332c2d6f213b0c1716e29
Andrey Popov <rainfinder@gmail.com>
2016-02-24 23:09:55 +0100

    Update plugin to reweight for lepton scale factors to new readers

---
+++ include/PECFwk/extensions/LeptonSFWeight.hpp
@@ -0,0 +1,102 @@
+#pragma once
+
+#include <PECFwk/extensions/EventWeightPlugin.hpp>
+
+#include <PECFwk/core/PhysicsObjects.hpp>
+
+#include <TH2D.h>
+
+#include <list>
+#include <memory>
+
+
+class LeptonReader;
+
+
+/**
+ * \class LeptonSFWeight
+ * \brief A plugin to reweight events to account for lepton scale factors
+ * 
+ * The scale factors are provided as one or more 2D histograms, which encode dependence on lepton
+ * transverse momentum and signed (!) pseudorapidity. If several histograms are provided, scale
+ * factors read from each of them are multiplied together. The user specifies which lepton flavour
+ * is considered. If an event contains several leptons of that flavour (accessed through a
+ * LeptonReader with a default name "Leptons"), the final event weight is a multiplication of scale
+ * factors for all of the leptons.
+ * 
+ * Currently no systematic uncertainty is evaluated.
+ */
+class LeptonSFWeight: public EventWeightPlugin
+{
+public:
+    /**
+     * \brief Constructor
+     * 
+     * The arguments are: name of the plugin, lepton flavour to be considered, name of the ROOT
+     * file containing 2D histograms with scale factors, and names of the histograms. The file name
+     * is resolved with the help of FileInPath, with a postfix "LeptonSF/". If one of the histogram
+     * is not found, the constructor throws an exception.
+     */
+    LeptonSFWeight(std::string const &name, Lepton::Flavour targetFlavour,
+      std::string const &srcFileName, std::list<std::string> const &histogramNames);
+    
+    /// A short-cut for the above version with a default name "LeptonSFWeight"
+    LeptonSFWeight(Lepton::Flavour targetFlavour, std::string const &srcFileName,
+      std::list<std::string> const &histogramNames);
+    
+    /// Default copy constructor
+    LeptonSFWeight(LeptonSFWeight const &) = default;
+    
+    /// Default move constructor
+    LeptonSFWeight(LeptonSFWeight &&) = default;
+    
+    /// Assignment operator is deleted
+    LeptonSFWeight &operator=(LeptonSFWeight const &) = delete;
+    
+    /// Trivial destructor
+    virtual ~LeptonSFWeight() noexcept;
+    
+public:
+    /**
+     * \brief Saves pointers to utilized plugins
+     * 
+     * Reimplemented from Plugin.
+     */
+    virtual void BeginRun(Dataset const &) override;
+    
+    /**
+     * \brief Creates a newly configured clone
+     * 
+     * Implemented from Plugin.
+     */
+    virtual Plugin *Clone() const override;
+    
+private:
+    /// Reads histograms with scale factors from the source file
+    void LoadScaleFactors(std::string const &srcFileName,
+      std::list<std::string> const &histogramNames);
+    
+    /**
+     * \brief Calculates the event weight from the scale factors
+     * 
+     * Implemented from Plugin.
+     */
+    virtual bool ProcessEvent() override;
+    
+private:
+    /// Name of the plugin that produces leptons
+    std::string leptonPluginName;
+    
+    /// Non-owning pointer to the plugin that produces leptons
+    LeptonReader const *leptonPlugin;
+    
+    /// Flavour of leptons, to which the scale factors should be applied
+    Lepton::Flavour targetFlavour;
+    
+    /**
+     * \brief Histograms with different components of scale factors
+     * 
+     * They are shared among all copies of this.
+     */
+    std::vector<std::shared_ptr<TH2D const>> sfComponents;
+};
---
+++ modules/extensions/src/LeptonSFWeight.cpp
@@ -0,0 +1,123 @@
+#include <PECFwk/extensions/LeptonSFWeight.hpp>
+
+#include <PECFwk/core/FileInPath.hpp>
+#include <PECFwk/core/LeptonReader.hpp>
+#include <PECFwk/core/Processor.hpp>
+#include <PECFwk/core/ROOTLock.hpp>
+
+#include <TFile.h>
+
+#include <sstream>
+#include <stdexcept>
+
+
+LeptonSFWeight::LeptonSFWeight(std::string const &name,
+  Lepton::Flavour targetFlavour_, std::string const &srcFileName,
+  std::list<std::string> const &histogramNames):
+    AnalysisPlugin(name),  // need to call constructor of a virtual base class explicitly
+    EventWeightPlugin(name),
+    leptonPluginName("Leptons"), leptonPlugin(nullptr),
+    targetFlavour(targetFlavour_)
+{
+    LoadScaleFactors(srcFileName, histogramNames);
+    
+    // The plugin will calculate one weight per event
+    EventWeightPlugin::weights.push_back(0.);
+}
+
+
+LeptonSFWeight::LeptonSFWeight(Lepton::Flavour targetFlavour_, std::string const &srcFileName,
+  std::list<std::string> const &histogramNames):
+    AnalysisPlugin("LeptonSFWeight"),
+    EventWeightPlugin("LeptonSFWeight"),
+    leptonPluginName("Leptons"), leptonPlugin(nullptr),
+    targetFlavour(targetFlavour_)
+{
+    LoadScaleFactors(srcFileName, histogramNames);
+    
+    // The plugin will calculate one weight per event
+    EventWeightPlugin::weights.push_back(0.);
+}
+
+
+LeptonSFWeight::~LeptonSFWeight() noexcept
+{}
+
+
+void LeptonSFWeight::BeginRun(Dataset const &)
+{
+    // Save pointers to the plugin that provides leptons
+    leptonPlugin = dynamic_cast<LeptonReader const *>(
+      GetMaster().GetPluginBefore(leptonPluginName, GetName()));
+}
+
+
+Plugin *LeptonSFWeight::Clone() const
+{
+    return new LeptonSFWeight(*this);
+}
+
+
+void LeptonSFWeight::LoadScaleFactors(std::string const &srcFileName,
+  std::list<std::string> const &histogramNames)
+{
+    // Open the source file and read the histograms with scale factors from it
+    ROOTLock::Lock();
+    
+    FileInPath pathBuilder;
+    std::string const srcFilePath(pathBuilder.Resolve("LeptonSF", srcFileName));
+    std::unique_ptr<TFile> srcFile(TFile::Open(srcFilePath.c_str()));
+    
+    for (std::string const &histName: histogramNames)
+    {
+        TH2D *hist = dynamic_cast<TH2D *>(srcFile->Get(histName.c_str()));
+        
+        if (not hist)
+        {
+            std::ostringstream message;
+            message << "LeptonSFWeight::LeptonSFWeight: Cannot find histogram \"" <<
+              histName << "\" in file \"" << srcFilePath << "\".";
+            
+            throw std::runtime_error(message.str());
+        }
+        
+        hist->SetDirectory(nullptr);
+        sfComponents.emplace_back(hist);
+    }
+    
+    srcFile.reset();
+    
+    ROOTLock::Unlock();
+}
+
+
+bool LeptonSFWeight::ProcessEvent()
+{
+    // A shortcut for tight leptons
+    auto const &leptons = leptonPlugin->GetLeptons();
+    
+    
+    // Loop over the leptons and calculate the total scale factor
+    double scaleFactor = 1.;
+    
+    for (auto const &lepton: leptons)
+    {
+        // Skip leptons of wrong flavour
+        if (lepton.GetFlavour() != targetFlavour)
+            continue;
+        
+        // Loop over components of the scale factor
+        for (auto const &hist: sfComponents)
+        {
+            int const bin = hist->FindFixBin(lepton.Pt(), lepton.Eta());
+            scaleFactor *= hist->GetBinContent(bin);
+        }
+    }
+    
+    
+    // Update the nominal weight
+    weights.at(0) = scaleFactor;
+    
+    
+    return true;
+}
