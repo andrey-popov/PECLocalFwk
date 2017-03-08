81428573c3274c1d0061f5fc007b44f23b988834
Andrey Popov <rainfinder@gmail.com>
2017-03-08 17:57:38 +0100

    Add a plugin to read trigger objects

---
+++ include/mensura/PECReader/PECTriggerObjectReader.hpp
@@ -0,0 +1,142 @@
+#pragma once
+
+#include <mensura/core/PhysicsObjects.hpp>
+#include <mensura/core/ReaderPlugin.hpp>
+
+#include <mensura/PECReader/Candidate.hpp>
+
+#include <initializer_list>
+#include <map>
+#include <regex>
+#include <string>
+#include <vector>
+
+
+class PECInputData;
+
+
+/**
+ * \class PECTriggerObjectReader
+ * \brief Reads trigger objects for selected filters
+ * 
+ * This plugin reads trigger objects that have been accepted by trigger filters. It is possible to
+ * select which of filters stored in a PEC file to read. The objects are represented by class
+ * Candidate.
+ */
+class PECTriggerObjectReader: public ReaderPlugin
+{
+public:
+    /**
+     * \brief Creates plugin with the given name
+     * 
+     * The plugin will only read trigger objects for filters whose names match at least one of the
+     * given mask. The masks are interpreted as regular expressions. By default a mask that matches
+     * any name is used.
+     */
+    PECTriggerObjectReader(std::string const &name,
+      std::initializer_list<std::string> triggerMasks = {".*"});
+    
+    /**
+     * \brief Crates plugin with default name "TriggerObjects"
+     * 
+     * See documentation for the first version of constructor for details.
+     */
+    PECTriggerObjectReader(std::initializer_list<std::string> triggerMasks = {".*"});
+    
+    /// Default copy constructor
+    PECTriggerObjectReader(PECTriggerObjectReader const &src) = default;
+    
+    /// Default move constructor
+    PECTriggerObjectReader(PECTriggerObjectReader &&) = default;
+    
+    /// Assignment operator is deleted
+    PECTriggerObjectReader &operator=(PECTriggerObjectReader const &) = delete;
+    
+public:
+    /**
+     * \brief Sets up reading of a tree with trigger objects
+     * 
+     * If the tree contains no branches that match at least one of the masks given to constructor,
+     * an exception is thrown.
+     * 
+     * Reimplemented from Plugin.
+     */
+    virtual void BeginRun(Dataset const &) override;
+    
+    /**
+     * \brief Creates a newly configured clone
+     * 
+     * Implemented from Plugin.
+     */
+    virtual Plugin *Clone() const override;
+    
+    /**
+     * \brief Returns index of the requested trigger filter for a fast access
+     * 
+     * Throws an exception if the filter is not known (because its name does not satisfy any of the
+     * masks given to constructor or because the input tree does not contain such branch).
+     */
+    unsigned GetFilterIndex(std::string const &triggerFilterName) const;
+    
+    /**
+     * \brief Returns collection of trigger objects for the given trigger filter
+     * 
+     * If the filter is not known (because its name does not satisfy any of the masks given to
+     * constructor or because the input tree does not contain such branch), an exception is thrown.
+     */
+    std::vector<Candidate> const &GetObjects(std::string const &triggerFilterName) const;
+    
+    /**
+     * \brief Returns collection of trigger objects for the filter with the given index
+     * 
+     * This is a bit faster version of the above method. Throws an exception if the given index is
+     * out of range.
+     */
+    std::vector<Candidate> const &GetObjects(unsigned filterIndex) const;
+    
+private:
+    /**
+     * \brief Reads trigger objects for selected filters from input tree
+     * 
+     * Reimplemented from Plugin.
+     */
+    virtual bool ProcessEvent() override;
+    
+private:
+    /// Name of the plugin that reads PEC files
+    std::string inputDataPluginName;
+    
+    /// Non-owning pointer to a plugin that reads PEC files
+    PECInputData const *inputDataPlugin;
+    
+    /// Name of the tree containing information about trigger objects
+    std::string treeName;
+    
+    /// Masks to choose trigger filters to read
+    std::vector<std::regex> triggerMasks;
+    
+    /// Mapping from trigger filter names to buffer indices
+    std::map<std::string, unsigned> triggerIndexMap;
+    
+    /**
+     * \brief Vector of buffers to read collections of trigger objects
+     * 
+     * Indices of the outer vector are stored in triggerIndexMap.
+     */
+    std::vector<std::vector<pec::Candidate>> buffers;
+    
+    /**
+     * \brief Auxiliary pointers to buffers
+     * 
+     * Need by ROOT to read the objects from a tree. Indices of the outer vector are stored in
+     * triggerIndexMap.
+     */
+    std::vector<std::vector<pec::Candidate> *> bufferPointers;
+    
+    /**
+     * \brief Collections of trigger objects translated into standard classes
+     * 
+     * Indices of the outer vector are stored in triggerIndexMap.
+     */
+    std::vector<std::vector<Candidate>> triggerObjects;
+};
---
+++ modules/PECReader/src/PECTriggerObjectReader.cpp
@@ -0,0 +1,169 @@
+#include <mensura/PECReader/PECTriggerObjectReader.hpp>
+
+#include <mensura/core/Processor.hpp>
+#include <mensura/core/ROOTLock.hpp>
+#include <mensura/PECReader/PECInputData.hpp>
+
+#include <TTree.h>
+
+#include <sstream>
+#include <stdexcept>
+
+
+PECTriggerObjectReader::PECTriggerObjectReader(std::string const &name,
+  std::initializer_list<std::string> triggerMasks_ /*= {".*"}*/):
+    ReaderPlugin(name),
+    inputDataPluginName("InputData"), inputDataPlugin(nullptr),
+    treeName("pecTriggerObjects/TriggerObjects")
+{
+    // Build trigger masks
+    triggerMasks.reserve(triggerMasks_.size());
+    
+    for (auto const &mask: triggerMasks_)
+        triggerMasks.emplace_back(mask);
+}
+
+
+PECTriggerObjectReader::PECTriggerObjectReader(
+  std::initializer_list<std::string> triggerMasks_ /*= {".*"}*/):
+    PECTriggerObjectReader("TriggerObjects", triggerMasks_)
+{}
+
+
+void PECTriggerObjectReader::BeginRun(Dataset const &)
+{
+    // Save pointer to the plugin that provides access to input data
+    inputDataPlugin = dynamic_cast<PECInputData const *>(GetDependencyPlugin(inputDataPluginName));
+    
+    
+    // Set up reading of the tree with trigger objects
+    inputDataPlugin->LoadTree(treeName);
+    
+    ROOTLock::Lock();
+    TTree *t = inputDataPlugin->ExposeTree(treeName);
+    
+    
+    // Find out what branches should be read. Their names are put into the trigger index map.
+    TObjArray const &branches = *t->GetListOfBranches();
+    triggerIndexMap.clear();
+    unsigned index = 0;
+    
+    for (unsigned iBranch = 0; iBranch < unsigned(branches.GetEntries()); ++iBranch)
+    {
+        std::string const branchName(branches[iBranch]->GetName());
+        bool matched = false;
+        
+        for (auto const &mask: triggerMasks)
+            if (std::regex_match(branchName, mask))
+            {
+                matched = true;
+                break;
+            }
+        
+        if (matched)
+        {
+            triggerIndexMap[branchName] = index;
+            ++index;
+        }
+    }
+    
+    
+    if (triggerIndexMap.size() == 0)
+    {
+        std::ostringstream message;
+        message << "PECTriggerObjectReader[\"" << GetName() << "\"]::BeginRun: "
+          "None of stored trigger filters match provided mask.";
+        throw std::runtime_error(message.str());
+    }
+    
+    
+    // Set buffers
+    buffers.resize(triggerIndexMap.size());
+    bufferPointers.resize(triggerIndexMap.size());
+    
+    t->SetBranchStatus("*", false);
+    
+    for (auto const &p: triggerIndexMap)
+    {
+        bufferPointers.at(p.second) = &buffers.at(p.second);
+        t->SetBranchStatus((p.first + ".*").c_str(), true);
+        t->SetBranchAddress(p.first.c_str(), &bufferPointers.at(p.second));
+    }
+    
+    ROOTLock::Unlock();
+    
+    
+    triggerObjects.resize(triggerIndexMap.size());
+}
+
+
+Plugin *PECTriggerObjectReader::Clone() const
+{
+    return new PECTriggerObjectReader(*this);
+}
+
+
+unsigned PECTriggerObjectReader::GetFilterIndex(std::string const &triggerFilterName) const
+{
+    auto const res = triggerIndexMap.find(triggerFilterName);
+    
+    if (res == triggerIndexMap.end())
+    {
+        std::ostringstream message;
+        message << "PECTriggerObjectReader[\"" << GetName() << "\"]::GetFilterIndex: "
+          "Trigger filter \"" << triggerFilterName << "\" has not been booked.";
+        throw std::runtime_error(message.str());
+    }
+    
+    return res->second;
+}
+
+
+std::vector<Candidate> const &PECTriggerObjectReader::GetObjects(
+  std::string const &triggerFilterName) const
+{
+    return GetObjects(GetFilterIndex(triggerFilterName));
+}
+
+
+std::vector<Candidate> const &PECTriggerObjectReader::GetObjects(unsigned filterIndex) const
+{
+    if (filterIndex >= triggerObjects.size())
+    {
+        std::ostringstream message;
+        message << "PECTriggerObjectReader[\"" << GetName() << "\"]::GetObjects: "
+          "Given index " << filterIndex << " is out out range.";
+        throw std::runtime_error(message.str());
+    }
+    
+    return triggerObjects[filterIndex];
+}
+
+
+bool PECTriggerObjectReader::ProcessEvent()
+{
+    inputDataPlugin->ReadEventFromTree(treeName);
+    
+    
+    for (unsigned iTrigger = 0; iTrigger < buffers.size(); ++iTrigger)
+    {
+        auto &curTriggerObjects = triggerObjects[iTrigger];
+        
+        // Clear from previous event
+        curTriggerObjects.clear();
+        
+        
+        // Translate PEC classes into standard representation
+        for (auto const &srcObject: buffers[iTrigger])
+        {
+            Candidate translated;
+            translated.SetPtEtaPhiM(srcObject.Pt(), srcObject.Eta(), srcObject.Phi(),
+              srcObject.M());
+            curTriggerObjects.emplace_back(translated);
+        }
+    }
+    
+    
+    // Always return true since this reader does not know when the input file is exhausted
+    return true;
+}
